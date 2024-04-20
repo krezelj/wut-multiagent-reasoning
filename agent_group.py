7ec2ed0ed5694e5981123005c70d77df28eda1a6
Jasiek <jasiekk9@gmail.com>
2024-04-20 22:02:37 +0200

    Add super group and repr functions

--- agent_group.py
+++ agent_group.py
@@ -3,8 +3,22 @@ from expressions import Node
 
 class AgentGroup:
 
-    def __init__(self, values: dict) -> None:
-        self.values = values
+    def __init__(self, agents: list[str], all_agents: list[str]) -> None:
+        self.values = {a: a in agents for a in all_agents}
 
-    def models(self, condition: Node) -> True:
+    def models(self, condition: Node) -> bool:
         return condition(self.values)
+    
+    def __repr__(self) -> str:
+        return str(self.values)
+
+class SuperGroup(AgentGroup):
+
+    def __init__(self) -> None:
+        pass
+
+    def models(self, condition: Node) -> bool:
+        return True
+    
+    def __repr__(self) -> str:
+        return "SuperGroup"
