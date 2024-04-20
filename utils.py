0f500e6a47ea561659d27af39590fb642a06797c
Jasiek <jasiekk9@gmail.com>
2024-04-20 07:45:55 +0200

    Add utils

---
+++ utils.py
@@ -0,0 +1,19 @@
+def enc2values(enc: int, literals: list[str]) -> dict:
+    values = {}
+    for l in literals[::-1]:
+        values[l] = (enc & 1) == 1
+        enc >>= 1
+    return values
+
+
+
+def values2enc(values: dict, literals: list[str]) -> int:
+    enc = 0
+    for l in literals:
+        enc <<= 1
+        enc |= values[l]
+    return enc
+
+
+def is_strict_subset(a: int, b: int) -> bool:
+    return (a | b == b) and (a != b)
