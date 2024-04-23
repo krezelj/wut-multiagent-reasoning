b9652780b1c4e67b88ff45f951a9bd0d0bd6156c
Jasiek <jasiekk9@gmail.com>
2024-04-23 07:09:03 +0200

    Add graph generation to model

--- core/model.py
+++ core/model.py
@@ -7,13 +7,17 @@ from core.agent_group import *
 
 class Model:
 
-    def __init__(self, fluents: list[str], domain) -> None:
+    def __init__(self, fluents: list[str], actions: list[str], agents: list[str], domain) -> None:
         self.fluents = [f.lower() for f in fluents]
+        self.actions = actions
+        self.agents = agents
         self.domain = domain
+
         self.split_statements()
         self.generate_states()
         self.set_nonintertial()
         self.set_initial_states()
+        self.graph = Graph(self)
 
     def split_statements(self) -> None:
         self.initialisation_statements: list[InitialisationStatement] = \
@@ -54,7 +58,7 @@ class Model:
         if len(self.initial_states) == 0:
             raise Exception("No viable initials states")
 
-    def res(self, action: str, agents: AgentGroup, state: State) -> list[State]:
+    def res(self, action: str, agents: AgentGroup, state: State, force_execution: bool = False) -> list[State]:
         # get all applicable effect and release statements
         is_statement_applicable = \
             lambda e : state.models(e.pre_condition) and agents.models(e.agent_condition) and e.action == action.lower()
@@ -66,7 +70,7 @@ class Model:
         # after the minimisation they will be reduced to only the current state (unless some releases also happen)
         post_condition = And([e.post_condition for e in effects])
         res0 = [s for s in self.states if s.models(post_condition)]
-        if len(res0) == 0:
+        if len(res0) == 0 and not force_execution:
             raise Exception(f"Unexecutable action {action} by {agents} in {state}")
         
         # get released fluents
@@ -91,4 +95,31 @@ class Model:
     
     def get_states_that_model(self, condition):
         return [s for s in self.states if s.models(condition)]
+
+
+class GraphNode:
+
+        def __init__(self, state: State) -> None:
+            self.state = state
+            self.neighbours: list[tuple[str, int, GraphNode]] = []
+
+        def find_neighbours(self, model: Model, nodes: list['GraphNode']) -> None:
+            for action in model.actions:
+                for i in range(2**len(model.agents)):
+                    values = enc2values(i, model.agents)
+                    agents = AgentGroup(values, model.agents)
+                    res = model.res(action, agents, self.state, force_execution=True)
+                    for state in res:
+                        node = nodes[state.encoding]
+                        self.neighbours.append((action, i, node))
+
+
+class Graph:
+
+
+    def __init__(self, model: Model) -> None:
+        self.nodes: list[GraphNode] = [GraphNode(state) for state in model.states]
+        for node in self.nodes:
+            node.find_neighbours(model, self.nodes)
+
     
