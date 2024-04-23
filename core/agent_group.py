57a99daba5a2bc7c756a01ccf031fd016657c770
Jasiek <jasiekk9@gmail.com>
2024-04-23 07:08:50 +0200

    Modify agent group constructor

--- core/agent_group.py
+++ core/agent_group.py
@@ -1,16 +1,23 @@
+from typing import Union
 from core.expressions import Node
+from core.utils import *
 
 
 class AgentGroup:
 
-    def __init__(self, agents: list[str], all_agents: list[str]) -> None:
-        self.values = {a: a in agents for a in all_agents}
+    def __init__(self, values: Union[dict, list[str]], all_agents: list[str]) -> None:
+        if isinstance(values, dict):
+            self.values = values
+        else:
+            self.values = {a: a in values for a in all_agents}
+        self.encoding = values2enc(self.values, all_agents)
 
     def models(self, condition: Node) -> bool:
         return condition(self.values)
     
     def __repr__(self) -> str:
         return str(self.values)
+
 
 class SuperGroup(AgentGroup):
 
