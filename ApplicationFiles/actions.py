5131efdae9821fc3dce1cf084a9c71449ee1f929
Jakub Wasilewski <jakub.wasilewski@oracle.com>
2024-04-26 18:59:49 +0200

    Walidacja sygnatury

--- ApplicationFiles/actions.py
+++ ApplicationFiles/actions.py
@@ -1,8 +1,42 @@
-
+import re
+from ApplicationFiles.errors import *
 def readSignature(signature, FluentInput, ActionsInput, AgentsInput):
-    # data must be separated by ', ' (comma + space).
+    # data must be separated by ',' (comma + space).
+    # tutaj może bym dodał jeszcze obsługę żeby nie było tak że jakiś obiekt ma spacje.
+    # Żeby nie było że użytkownik poda: nazwa1, nazwa 2
+    
+    def clearWord(text):
+        return text.strip()
     def extractData(text):
-        text.split(", ")
-    signature.fluents = extractData(FluentInput.get())
-    signature.actions = extractData(ActionsInput.get())
-    signature.fluents = extractData(AgentsInput.get())
+        return text.split(",")
+    
+    def validateList(txt_list):
+        for word in txt_list:
+            if(word.isspace()):
+                raise SpaceException()
+            if(word == ''):
+                raise EmptyWordException()
+            if(word.isdigit()):
+                raise DigitWordException()
+
+    signature.fluents = [clearWord(i) for i in extractData(FluentInput.get())]
+    signature.actions = [clearWord(i) for i in extractData(ActionsInput.get())]
+    signature.agents = [clearWord(i) for i in extractData(AgentsInput.get())]
+    
+    def signatureValidator(list, name):
+        try:
+            validateList(list)
+        except SpaceException as se:
+            raise Exception(name + " can not be multi-word")
+        except DigitWordException as dwe:
+            raise Exception(name + " can not be a digit")
+        except EmptyWordException as ee:
+            raise Exception(name + " can not be empty word")
+    try:
+        signatureValidator(signature.fluents, "Fluent")
+        if clearWord(ActionsInput.get()) != '':
+            signatureValidator(signature.actions, "Action")
+        if clearWord(AgentsInput.get()) != '':
+            signatureValidator(signature.agents, "Agent")
+    except Exception as e:
+        raise e
