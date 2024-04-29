7b2971611d7bed13b68a1bc380a041fab118a80f
kubawini <kubawini@op.pl>
2024-04-29 22:37:13 +0200

    Add noninertial template

--- ApplicationFiles/statement_forms.py
+++ ApplicationFiles/statement_forms.py
@@ -3,6 +3,7 @@ from tkinter import *
 from ApplicationFiles.statement_form.AlwaysForm import AlwaysForm
 from ApplicationFiles.statement_form.EffectForm import EffectForm
 from ApplicationFiles.statement_form.ImpossibleForm import ImpossibleForm
+from ApplicationFiles.statement_form.NoninertialForm import NoninertialForm
 from ApplicationFiles.statement_form.ReleaseForm import ReleaseForm
 
 class StatementForms:
@@ -13,7 +14,8 @@ class StatementForms:
             "Impossible": ImpossibleForm(root, statements_listbox),
             "Effect": EffectForm(root, statements_listbox),
             "Release": ReleaseForm(root, statements_listbox),
-            "Always": AlwaysForm(root, statements_listbox)
+            "Always": AlwaysForm(root, statements_listbox),
+            "Noninertial": NoninertialForm(root, statements_listbox),
         }
         self.current_form = None
         # Create a frame for the statement controls
