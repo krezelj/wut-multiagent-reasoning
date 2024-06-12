290bc718915dc3fb708123050f3bd1f1a31c2a95
Filip <fillipser143@gmail.com>
2024-06-12 20:37:04 +0200

    Queries forms - initial

--- Application.py
+++ Application.py
@@ -1,6 +1,8 @@
 import tkinter
 from tkinter import *
 from tkinter.font import Font
+
+from ApplicationFiles.queries_forms import QueriesForms
 from core.model import *
 from ApplicationFiles.properties import *
 from ApplicationFiles.actions import *
@@ -48,9 +50,9 @@ default_font = Font(size=10)
 # Set the default font for labels and inputs
 root.option_add("*Label.font", default_font)
 root.option_add("*Entry.font", default_font)
-setProperties(root = root)
+setProperties(root=root)
 
-FluentInput, ActionsInput, AgentsInput, SignatureWarningLabel = signatureSection(root = root)
+FluentInput, ActionsInput, AgentsInput, SignatureWarningLabel = signatureSection(root=root)
 signature = Signature()
 # Create a frame for the listbox
 listbox_frame = LabelFrame(root, text="Statements", width=500, font=(15))  # Set a fixed width
@@ -64,22 +66,23 @@ statements_listbox.objects = []
 
 # Create an instance of StatementForms
 statement_forms = StatementForms(root, statements_listbox)
+queries_forms = QueriesForms(root, statements_listbox)
 
 
 def signatureAction():
     SignatureWarningLabel.config(foreground="red")
     try:
-         readSignature(signature, FluentInput, ActionsInput, AgentsInput)
+        readSignature(signature, FluentInput, ActionsInput, AgentsInput)
+        queries_forms.set_agents(signature.agents)
     except Exception as e:
         SignatureWarningLabel.config(text=e.args[0])
         return
-    SignatureWarningLabel.config(text="Correct Signature",foreground="green")
-    
+    SignatureWarningLabel.config(text="Correct Signature", foreground="green")
 
-signatureButton = Button(root, text="Apply Signature", command = signatureAction)
+
+signatureButton = Button(root, text="Apply Signature", command=signatureAction)
 signatureButton.config(width=15)
 signatureButton.grid(row=1, column=1)
-
 
 # TODO: Create model from user input instead of this stub
 fluents = ['p', 'd']
---
+++ ApplicationFiles/queries_forms.py
@@ -0,0 +1,47 @@
+from tkinter import *
+
+from ApplicationFiles.query_forms.ExecutableQueryForm import ExecutableQueryForm
+
+
+class QueriesForms:
+    def __init__(self, root, statements_listbox):
+        self.agents = None
+        self.root = root
+        self.statements_listbox = statements_listbox
+
+        # Create a new frame for the query forms
+        self.query_forms_frame = Frame(root)
+        self.query_forms_frame.grid(row=4, column=1, sticky='nsew')  # Adjust the row and column as needed
+        self.query_types = {
+            "Executable": ExecutableQueryForm(self.query_forms_frame),
+        }
+        self.current_form = None
+        # Create a frame for the statement controls
+        self.statement_controls_frame = LabelFrame(root, text="Query Controls", font=15)
+        self.statement_controls_frame.grid(row=3, column=1, sticky='ew', pady=(10, 0))
+
+        # Create a dropdown menu for the statement types
+        self.query_type_var = StringVar()
+        self.query_type_var.set("Select query type")  # Set the initial value to the label text
+        self.query_type_dropdown = OptionMenu(self.statement_controls_frame, self.query_type_var, *self.query_types.keys(), command=self.show_form)
+        self.query_type_dropdown.config(width=20)
+        self.query_type_dropdown.grid(row=0, column=0, sticky='e')
+        self.answer_button = Button(self.statement_controls_frame, text="Answer", command=self.answer_query)
+        self.answer_button.grid(row=0, column=1, sticky='w')
+
+    def answer_query(self):
+        query = self.current_form.answer_query(self.agents)
+        print(query)
+
+    def set_agents(self, agents):
+        self.agents = agents
+
+    def show_form(self, query_type):
+        # Hide the current form if it exists
+        if self.current_form is not None:
+            self.current_form.hide()
+
+        # Show the selected form
+        self.current_form = self.query_types[query_type]
+        self.current_form.show()
+        self.query_type_var.set("Select query type")  # Set the initial value to the label text
